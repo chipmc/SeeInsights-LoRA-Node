0c6a506bbe6106a89391a34a10a9ef5e40b04604
orbitllc <alexbowen@orbitllc.site>
2023-12-09 20:27:07 -0500

    removed debugging print

--- src/LoRA-Node-Occupancy.cpp
+++ src/LoRA-Node-Occupancy.cpp
@@ -124,7 +124,6 @@ void loop()
 			static unsigned long keepAwake = 0;
 			if (state != oldState) {
 				keepAwake = millis();
-				Log.infoln("%d", keepAwake);
 				publishStateTransition();              							// We will apply the back-offs before sending to ERROR state - so if we are here we will take action
 			}
 			if (currentData.currentDataChanged && timeFunctions.getTime() - sysStatus.lastConnection > TRANSMIT_LATENCY) {	// If the current data has changed and we have not connected in the last minute
@@ -133,7 +132,7 @@ void loop()
 			}
 			else if (sysStatus.alertCodeNode != 0) state = ERROR_STATE;			// If there is an alert code, we need to resolve it
 			else if (sensorDetect) state = ACTIVE_PING;							// Someone is in the door start pinging
-			else if (millis() - keepAwake > 1000) state = SLEEPING_STATE;	    // If nothing else, go back to sleep - disabled until we get the PIR sensor - keep awake for 1 second 
+			// else if (millis() - keepAwake > 1000) state = SLEEPING_STATE;	    // If nothing else, go back to sleep - disabled until we get the PIR sensor - keep awake for 1 second 
 		} break;
 
 		case SLEEPING_STATE: {
