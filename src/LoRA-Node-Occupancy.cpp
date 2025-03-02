e5b1e3e3b8286788dfd5e48d04d10548efb53335
orbitllc <gabowen@ncsu.edu>
2025-03-02 11:29:50 -0500

    fixed bad copy paste

--- src/LoRA-Node-Occupancy.cpp
+++ src/LoRA-Node-Occupancy.cpp
@@ -424,7 +424,7 @@ void loop()
 			break;
 			case 13: 															// This state sets the value of the current net count to the value sent in the alert context on the gateway data acknowledgement
 				sysStatus.tofDetectionsPerSecond = sysStatus.alertContextNode;
-				Log.infoln("Alert code 13 - TOF Sensor Polling Rate now set to %dms", sysStatus.alertContextNode);
+				Log.infoln("Alert code 13 - TOF Sensor Polling Rate now set to %dms", sysStatus.tofDetectionsPerSecond);
 				sysStatus.alertCodeNode = 0;
 				state = LoRA_TRANSMISSION_STATE;								// Sends the alert and clears alert code
 			break;
